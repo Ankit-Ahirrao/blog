778df7c9db32f2324cab9e198db04f30f2c50011
Ankit Ahirrao <aahirrao@bestpeers.com>
2022-05-19 19:16:17 +0530

    files modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
   root "articles#index"
+  
+  #get '/article', action: :show, controller: 'articles'
   #get "/articles", to: "articles#index"
   #get "/articles/:id", to: "articles#show"
+  #get "/search", to: "articles#index"
   resources :articles do
     resources :comments
   end
+ #resources :articles, only: [:index, :show]
 end
