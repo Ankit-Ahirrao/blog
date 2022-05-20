d8ab9949c930ca78968432742b28b106bb9fad28
Ankit Ahirrao <aahirrao@bestpeers.com>
2022-05-20 19:51:42 +0530

    callbacks added

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,14 @@ class Article < ApplicationRecord
 
     has_many :comments, dependent: :destroy
     #validates :termsofservice, acceptance: true
-    validates :title, presence: { message: "Title must be given"}
+    validates :title, presence: true
+    before_validation :ensure_title_has_value 
+
+    def ensure_title_has_value
+        if title.blank?
+            self.title = body unless body.blank?
+        end
+    end
     validates :body, presence: true, length: { minimum: 10 }
+
 end
